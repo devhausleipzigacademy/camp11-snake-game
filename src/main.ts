783672dee6cfdd2f11af728da9cb2da4ee47e10d
Franz Wollang <franzwollang@gmail.com>
2023-12-05 15:13:38 +0100

    Basic scaffolding

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-const gameElement = document.getElementById("game")!;
+const gameGrid = document.getElementById("game-grid");
 
-gameElement.style.color = `red`;
-gameElement.style.fontSize = `50px`;
+const rows = 21;
+const columns = 21;
+
+// loop over the rows
+// // loop over the columns
+// // // create a div
+// // // add a styling (class) to the div
+// // // append the div to the game grid
