c3fd92af370ce777415072020fe8fe09950f1963
Franz Wollang <franzwollang@gmail.com>
2023-12-05 16:26:00 +0100

    Added grid fill/reset; snake/apple data structures; entity rendering

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,48 @@
-const gameGrid = document.getElementById("game-grid");
+const gameGrid = document.getElementById("game-grid")!;
 
 const rows = 21;
 const columns = 21;
 
-// loop over the rows
-// // loop over the columns
-// // // create a div
-// // // add a styling (class) to the div
-// // // append the div to the game grid
+function fillGrid() {
+	for (let row = 1; row <= 21; row++) {
+		for (let col = 1; col <= 21; col++) {
+			const newGridElement = document.createElement("div");
+			newGridElement.id = `grid-${row}-${col}`;
+
+			newGridElement.classList.add("grid-element");
+			newGridElement.textContent = `${row}-${col}`;
+			gameGrid.append(newGridElement);
+		}
+	}
+}
+
+function emptyGrid() {
+	while (gameGrid.firstChild) {
+		gameGrid.removeChild(gameGrid.firstChild);
+	}
+}
+
+function resetGrid() {
+	emptyGrid();
+	fillGrid();
+}
+
+// Game State
+const snake = ["10-11", "11-11", "12-11"];
+const apples = ["2-2"];
+
+function renderEntities() {
+	styleElements(snake, "snake");
+	styleElements(apples, "apple");
+}
+
+function styleElements(elements: string[], style: string) {
+	elements.forEach((element) => {
+		const gridElement = document.getElementById(`grid-${element}`);
+		gridElement?.classList.add(style);
+	});
+}
+
+// initialize Game
+fillGrid();
+renderEntities();
